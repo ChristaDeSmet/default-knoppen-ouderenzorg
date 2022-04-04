610eb97f0b26678e666b3bd1f4654713097cd3e4
ChristaDeSmet <christa.desmet@cevi.be>
2022-04-04 16:22:09 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.less' ]
 })
-export class AppComponent  {
+
+export class AppComponent implements OnInit {
   name = 'Angular';
-  sw_memo_false = false;
-  sw_memo_true = true;
+  sw_memo_false : boolean;
+  sw_memo_true : boolean ;
+  sw_memo : boolean;
+
+
+  ngOnInit() {
+    this.sw_memo_false = false;
+    this.sw_memo_true = true;
+    this.sw_memo = false;
+}
+
+getBoolClass(){
+  return '';
+}
 }
